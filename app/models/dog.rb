c2f38682a80e729dd94b1825128feb284d79d586
louis baptiste <louisvis@ymail.com>
2019-02-23 03:13:07 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,10 +1,10 @@
 class Dog
 attr_accessor :name, :age , :age
-  def initialize (name:, breed:,age:)
+  def initialize (name, breed,age)
     @name = name
     @breed = breed
     @age= age
-        
+
   end
 
 
